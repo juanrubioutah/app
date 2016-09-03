a5ccfcb13969a233f1fa6ace44e89542f3fce007
juanrubioutah <juanrubioutah@gmail.com>
2016-09-03 15:17:29 -0600

    Test Commit

--- App/core/src/com/zenapps/game/Main.java
+++ App/core/src/com/zenapps/game/Main.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
+//Juan is Cool
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
